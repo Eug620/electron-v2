fbd91ddf19020ffaf965a99bd0f07d11f9212785
yeyuhang <yeyuhang@appicplay.com>
2020-11-16 10:37:22 +0800

    add quasar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
+<!--
+ * @Author: yeyuhang
+ * @Date: 2020-11-07 12:32:28
+ * @LastEditTime: 2020-11-13 19:47:45
+ * @LastEditors: yeyuhang
+ * @Descripttion: 头部注释
+-->
 <template>
   <div id="app">
-    app
-    <!-- <router-view/> -->
+    <HelloWorld/>
   </div>
 </template>
 
 <script>
+import HelloWorld from "./components/HelloWorld";
 export default {
   name: 'App',
   components: {
+    HelloWorld
+  },
+  data () {
+    return {
+    }
   }
 }
 </script>
@@ -18,8 +30,10 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /* text-align: center; */
+  /* color: #2c3e50; */
+  /* margin-top: 60px; */
+  width: 100%;
+  height: 100%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
+/*
+ * @Author: yeyuhang
+ * @Date: 2020-11-07 12:32:28
+ * @LastEditTime: 2020-11-13 19:53:40
+ * @LastEditors: yeyuhang
+ * @Descripttion: 头部注释
+ */
 import Vue from 'vue'
 import App from './App.vue'
+import { Quasar, QIcon } from 'quasar'
+import './assets/style/base.scss'
+import './styles/quasar.sass'
 
 Vue.config.productionTip = false
-
+// Vue.prototype.$q = Quasar
+Vue.use(Quasar, {
+  components: {
+    QIcon
+  }
+})
 new Vue({
   render: h => h(App),
 }).$mount('#app')
